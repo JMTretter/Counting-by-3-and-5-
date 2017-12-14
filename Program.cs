ec0e83f4f2990c9989e33c2b310f176867a97723
jmtretter.jb <jmtretter.jb@hotmail.com>
2017-12-13 19:30:17 -0600

    Pushing to GitHub

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CountingBy3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            
+            for (num = 0; num <= 100;  num++)
+            {
+                if (num % 5 == 0 & num % 3 == 0)
+                    Console.WriteLine(num + " FizzBuzz");
+
+                if (num % 3 == 0)
+                    Console.WriteLine(num + " Fizz");
+                
+                if (num % 5 == 0)
+                    Console.WriteLine(num + " Buzz");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
